ed2ffa6d12cdbd8a8c08d492e4bb7274c2cc914e
raman pratap <ramanprtap>
2020-06-02 14:28:59 +0530

    Updates

---
+++ precache-manifest.8c63f22812a1323315be00d1b600ec48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74329fb24aa8c4ad0e6c4d1400a4893c",
+    "url": "/mausam/index.html"
+  },
+  {
+    "revision": "a657b90756762809d44e",
+    "url": "/mausam/static/css/main.bd0928c1.chunk.css"
+  },
+  {
+    "revision": "fd072381568976bde12e",
+    "url": "/mausam/static/js/2.435c8980.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mausam/static/js/2.435c8980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a657b90756762809d44e",
+    "url": "/mausam/static/js/main.782da40b.chunk.js"
+  },
+  {
+    "revision": "dd96c866fbebfa541994",
+    "url": "/mausam/static/js/runtime-main.b6df74ef.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/mausam/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/mausam/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
